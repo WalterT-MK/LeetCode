f681e937e9058bcfdf53d2ea8403fca85b3eb88c
Walter Tang <tangtgs1516993@gmail.com>
2021-12-11 02:04:59 -0800

    Time: 68 ms (95.71%), Space: 40 MB (39.32%) - LeetHub

---
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let i = 0;
+    let j = nums.length - 1;
+    while (i <= j) {
+        let mid = Math.floor((i+j)/2);
+        
+        if(nums[mid] === target) {
+            return mid;
+        }
+        
+        if (nums[i] <= nums[mid]) {
+            if (nums[i] <= target && target <= nums[mid]) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[j]) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
