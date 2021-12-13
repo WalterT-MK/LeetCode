d37ca40f9ea85c9d9dd335280eed96b9e0c242a7
Walter Tang <tangtgs1516993@gmail.com>
2021-12-12 21:58:21 -0800

    Time: 72 ms (91.52%), Space: 40.5 MB (38.79%) - LeetHub

---
+++ maximum-product-subarray/maximum-product-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    var preMax = nums[0];
+    var preMin = nums[0];
+    var ans = nums[0];
+    for (var i = 1; i < nums.length; i++) {
+        let currMax = Math.max(preMax*nums[i], nums[i], preMin*nums[i]);
+        let currMin = Math.min(preMax*nums[i], nums[i], preMin*nums[i]);
+        preMax = currMax;
+        preMin = currMin;
+        if (preMax > ans) {
+            ans = preMax;
+        }
+    }
+    return ans;
+};
