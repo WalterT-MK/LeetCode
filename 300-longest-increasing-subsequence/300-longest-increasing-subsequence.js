42b07819c492518cfff3a9aeec31d6b7781a50ad
Walter Tang <tangtgs1516993@gmail.com>
2022-03-07 13:50:27 -0800

    Time: 198 ms (58.22%), Space: 44.2 MB (26.95%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    var lis = [];
+    for (var i = 0; i < nums.length; i++) {
+        lis.push(1);
+        for (var j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                lis[i] = Math.max(lis[i], lis[j] + 1);
+            }
+        }
+    }
+    if (nums.length) {
+        return Math.max.apply(null, lis);
+    } else {
+        return 0;
+    }
+};
