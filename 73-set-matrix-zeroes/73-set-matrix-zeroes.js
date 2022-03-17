ea9f3bda733cd812f967a41e7c4c0dde64c0467a
Walter Tang <tangtgs1516993@gmail.com>
2022-03-16 18:24:46 -0700

    Time: 123 ms (44.55%), Space: 44.5 MB (73.38%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    var row = new Set();
+    var col = new Set();
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === 0) {
+                row.add(i);
+                col.add(j);
+            }
+        }
+    }
+    
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[0].length; j++) {
+            if (row.has(i) || col.has(j)) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+};
