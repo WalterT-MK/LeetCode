7f17e521e9849cd8373ad1bc6649fef91f44b982
Walter Tang <tangtgs1516993@gmail.com>
2022-03-16 21:50:14 -0700

    Time: 91 ms (36.13%), Space: 42.1 MB (25.95%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.js
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -3,23 +3,56 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    if (matrix.length === 0) return [];
-    if (matrix[0].length === 0) return [];
-    var output = [];
+//     if (matrix.length === 0) return [];
+//     if (matrix[0].length === 0) return [];
+//     var output = [];
     
-    output = output.concat(matrix.shift());
+//     output = output.concat(matrix.shift());
     
-    for (var i = 0; i < matrix.length - 1; i++) {
-        output.push(matrix[i].pop());
+//     for (var i = 0; i < matrix.length - 1; i++) {
+//         output.push(matrix[i].pop());
+//     }
+    
+//     var lastRow = matrix.pop();
+//     if (lastRow) output = output.concat(lastRow.reverse());
+    
+//     for (var i = matrix.length - 1; i > -1; i--) {
+//         if (matrix[i].length) output.push(matrix[i].shift());
+//     }
+    
+//     return output.concat(spiralOrder(matrix));
+    
+    var ans = [];
+    var sRow = 0;
+    var sCol = 0;
+    var eRow = matrix.length - 1;
+    var eCol = matrix[0].length - 1;
+    while (sRow <= eRow && sCol <= eCol) {
+        for (var i = sRow; i <= eCol; i++) {
+            ans.push(matrix[sRow][i]);
+        }
+        sRow++;
+        for (var i = sRow; i <= eRow; i++) {
+            ans.push(matrix[i][eCol]);
+        }
+        eCol--;
+        if (sRow <= eRow) {
+            var i = eCol;
+            while (i >= sCol) {
+                ans.push(matrix[eRow][i]);
+                i--;
+            }
+            eRow--;
+        }
+        if (sCol <= eCol) {
+            var i = eRow;
+            while (i >= sRow) {
+                ans.push(matrix[i][sCol]);
+                i--;
+            }
+            sCol++;
+        }
     }
-    
-    var lastRow = matrix.pop();
-    if (lastRow) output = output.concat(lastRow.reverse());
-    
-    for (var i = matrix.length - 1; i > -1; i--) {
-        if (matrix[i].length) output.push(matrix[i].shift());
-    }
-    
-    return output.concat(spiralOrder(matrix));
+    return ans;
 };
 
