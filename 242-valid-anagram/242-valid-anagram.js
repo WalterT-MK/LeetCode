ec8b7cc2cac583798942647fa6562aa20bde13ef
Walter Tang <tangtgs1516993@gmail.com>
2022-04-05 16:26:46 -0700

    Time: 94 ms (74.63%), Space: 43.3 MB (77.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,28 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length != t.length){
-        return false;
+    if (s.length !== t.length) return false;
+    let obj1 = {};
+    let obj2 = {};
+    for (let i = 0; i < s.length; i++) {
+        obj1[s[i]] = obj1[s[i]]+1 || 1;
+        obj2[t[i]] = obj2[t[i]]+1 || 1;
     }
-    const maps = new Map();
-    const mapt = new Map();
-    for (var i = 0; i<s.length; i++){
-        if(maps.has(s[i])) {
-            maps.set(s[i], maps.get(s[i]) + 1);
-        } else {
-            maps.set(s[i], 1);
-        }
-        if(mapt.has(t[i])) {
-            mapt.set(t[i], mapt.get(t[i]) + 1);
-        } else {
-            mapt.set(t[i], 1);
+    for (let i = 0; i < s.length; i++) {
+        if (obj1[s[i]] != obj2[s[i]]) {
+            return false
         }
     }
-    for(var [key, val] of maps) {
-        testVal = mapt.get(key);
-        if (testVal !== val || (testVal === undefined && !mapt.has(key))) {
-            return false;
-        }
-    }
-    return true
+    return true;
 };
