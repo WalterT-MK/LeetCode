9e469651174d6e2604ec034f9409a32cb7553141
Walter Tang <tangtgs1516993@gmail.com>
2022-04-05 17:04:28 -0700

    Time: 92 ms (75.40%), Space: 44.7 MB (84.92%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -6,9 +6,7 @@
 var topKFrequent = function(nums, k) {
     var map1 = new Map();
     var ans = [];
-    for (let num of nums) {
-        map1.set(num, map1.get(num) + 1 || 1);
-    }
+    nums.forEach(num => map1.set(num, map1.get(num)+1 || 1));
     var sortable = [...map1].sort((a,b) => b[1] - a[1]);
     for (let i = 0; i < k; i++) {
         ans.push(sortable[i][0])
