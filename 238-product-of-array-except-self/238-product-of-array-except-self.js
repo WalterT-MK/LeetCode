1684d3b8ea88eadff7588e23ca99f67625bae9ba
Walter Tang <tangtgs1516993@gmail.com>
2022-04-05 20:56:31 -0700

    Time: 1493 ms (5.07%), Space: 54.7 MB (51.01%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -6,50 +6,36 @@
 //with division
 var productExceptSelf = function(nums) {
     
+    // 1 2 3 4
+    // 1  2   6   24
+    // 24 24  12  4   
     
-    /*
-     1  2  3  4
-     1  1  2  6  24
-    24 24 12  4  1  
-    */
-//     var pre = [1];
-//     var pos = [];
-//     var ans = [];
-    
-//     for (var i = 1; i <= nums.length; i++) {
-//         pre.push(nums[i - 1] * pre[i - 1]);
-//     };
-//     pos[nums.length] = 1;
-//     for (var i = nums.length - 1; i >= 0; i--) {
-//         pos[i] = nums[i] * pos[i + 1];
-//     }
-//     for (var i = 0; i < nums.length; i++) {
-//         ans.push(pre[i]*pos[i+1]);
-//     }
-//     return ans;
-    var prefix = [nums[0]];
-    var postfix = new Array(nums.length-1).fill(0);
+    var arry1 = [];
+    var arry2 = [];
     var ans = [];
-    for (var i = 1; i < nums.length; i++) {
-        prefix.push(nums[i]*prefix[i-1]);
-    }
-    for (var i = nums.length-1; i >= 0; i--) {
-        if(i === nums.length - 1){
-            postfix[i] = nums[nums.length-1];
+    for (let i = 0; i < nums.length; i++) {
+        if (arry1.length === 0) {
+            arry1.push(nums[i]);
         } else {
-            postfix[i] = nums[i]*postfix[i+1];
+            arry1.push(nums[i] * arry1[i - 1]);
         }
     }
-    for (var i = 0; i < nums.length; i++) {
-        if (i === 0) {
-            ans.push(1*postfix[i+1]);
-        } else if(i === nums.length - 1) {
-            ans.push(prefix[i-1]*1);
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (arry2.length === 0) {
+            arry2.push(nums[i]);
         } else {
-            ans.push(prefix[i-1]*postfix[i+1]);
+            arry2.unshift(nums[i] * arry2[0]);
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (i === 0) {
+            ans.push(1 * arry2[i+1]);
+        } else if (i === nums.length - 1) {
+            ans.push(arry1[i - 1])
+        } else {
+            ans.push(arry1[i - 1] * arry2[i + 1])
         }
     }
     return ans;
-    
     
 };
