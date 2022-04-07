a000616575c4963e068011b223d0d9974068130a
Walter Tang <tangtgs1516993@gmail.com>
2022-04-07 14:50:12 -0700

    Time: 124 ms (38.95%), Space: 51.3 MB (93.16%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,27 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    // var price = prices[0];
-    // var profit = 0;
-    // for (var i = 1; i < prices.length; i++) {
-    //     if (prices[i] < price) {
-    //         price = prices[i];
-    //     } else {
-    //         var diff = prices[i] - price;
-    //         if (diff > profit)
-    //         profit = diff;
-    //     }
-    // }
-    // return profit;
-    var buyIn = prices[0];
-    var profit = 0;
-    for (var i = 0; i < prices.length; i++) {
-        if (prices[i] < buyIn) {
-            buyIn = prices[i];
+    let profit = 0;
+    let buyin = prices[0]
+    for (let i = 1; i < prices.length; i++) {
+        if(prices[i] < buyin) {
+            buyin = prices[i];
         } else {
-            var diff = prices[i] - buyIn;
-            if (diff > profit) {
-                profit = diff;
+            let currProfit = prices[i] - buyin;
+            if(currProfit > profit) {
+                profit = currProfit;
             }
         }
     }
