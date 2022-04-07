d12b767c96be822f1f97d94433225958b16ab45a
Walter Tang <tangtgs1516993@gmail.com>
2022-04-06 17:54:24 -0700

    Time: 122 ms (74.80%), Space: 50.6 MB (54.78%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -7,15 +7,14 @@ var longestConsecutive = function(nums) {
     const set1 = new Set(nums);
     let max = 0;
     for (let num of set1) {
-        if (set1.has(num - 1)) {
-            continue;
+        if (!set1.has(num - 1)) {
+            let length = 1;
+            let currnum = num;
+            while(set1.has(currnum + length)) {
+                length+=1;
+            }
+            max = Math.max(max, length);
         }
-        let length = 1;
-        let currnum = num;
-        while(set1.has(currnum + length)) {
-            length+=1;
-        }
-        max = Math.max(max, length);
     }
     return max;
 };
