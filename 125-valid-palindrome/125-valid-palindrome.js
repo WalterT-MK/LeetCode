d89d09c1a8ff8f09b84eb5262abf04f2b2b32f50
Walter Tang <tangtgs1516993@gmail.com>
2022-04-06 18:21:42 -0700

    Time: 96 ms (59.24%), Space: 44.2 MB (87.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,16 +4,13 @@
  */
 var isPalindrome = function(s) {
     var cleanS = s.replace(/[^0-9a-z]/gi,'').toLowerCase();
-    var left = 0;
-    var right = cleanS.length - 1;
-    console.log(cleanS)
-    while (left < right) {
-        if (cleanS[left] != cleanS[right]) {
-            return false;
-        } else {
-            left ++;
-            right --;
-        }
+    let i = 0; 
+    let j = cleanS.length - 1;
+    
+    while (i<j) {
+        if (cleanS[i]!==cleanS[j]) return false;
+        i+=1;
+        j-=1;
     }
     return true;
 };
