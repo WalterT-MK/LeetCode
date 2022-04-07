7dca46192f70805a664ee299dcc359afc46afdfd
Walter Tang <tangtgs1516993@gmail.com>
2022-04-06 20:28:02 -0700

    Time: 174 ms (75.41%), Space: 52.9 MB (52.02%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    var ans = [];
+    if (nums.length < 3) {
+        return ans;
+    }
+    nums.sort((a,b) => a - b);
+    for (let i = 0; i < nums.length-2; i++) {
+        if (nums[i] > 0) {
+            break;
+        }
+        if (i>0 && nums[i] === nums[i-1]) {
+            continue;
+        }
+        let l = i + 1; 
+        let r = nums.length - 1;
+        while (l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+            if(sum === 0){
+                ans.push([nums[i], nums[l], nums[r]]);
+                while(nums[l] === nums[l+1]) {
+                    l++;
+                }
+                while(nums[r] === nums[r-1]) {
+                    r--;
+                }
+                l++;
+                r--;
+            } else if (sum > 0) {
+                r--;
+            } else {
+                l++;
+            }
+
+        }
+    }
+    return ans;
+
+};
