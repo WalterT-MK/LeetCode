0e08b7ecdfb4331220ff01760ae2ee659dc8c92f
Walter Tang <tangtgs1516993@gmail.com>
2022-04-08 13:42:12 -0700

    Time: 94 ms (84.89%), Space: 47 MB (35.78%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.js
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -4,64 +4,40 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-//     if(t.length > s.length) return "";
-//     let mapt = new Map();
-//     for (let i = 0; i < t.length; i++) {
-//         mapt.set(t[i], mapt.get(t[i]) + 1 || 1);
-//     }
-//     let count = mapt.size;
-//     let min = '',
-//         l = 0,
-//         r = -1;
-//     while (r <= s.length) {
-//         console.log(count)
-//         if (count === 0) {
-//             let current = s[l];
-//             if (mapt.get(current)) mapt.set(current, mapt.get(current) + 1);
-//             if (mapt.get(current) > 0) count++;
-            
-//             let temp = s.substring(l,r+1);
-//             if(min.length < temp.length) min = temp;
-//             l++;
-//         } else {
-//             r++;
-//             let current = s[r];
-            
-//             if (mapt.get(current)) mapt.set(current, mapt.get(current) - 1);
-//             if (mapt.get(current) === 0) count--;
-//         }
-//     }
-//     return min;
+    let min = '',
+        set = {};
     
-    let min = "", left = 0, right = -1;
-    let map = {};
-    t.split('').forEach(element => {
-        if (map[element]==null) map[element] = 1;
-        else map[element] = map[element] + 1;
+    t.split('').forEach(char => {
+        if (set[char]) set[char]++;
+        else set[char] = 1;
     });
-    let count = Object.keys(map).length;
-
-    while (right < s.length) {
-		// found a valid substring
-        if (count == 0) {
-		
-            let current = s[left];
-            if (map[current] != null) map[current]++;
-            if (map[current] > 0) count++;    
-			
-            let temp = s.substring(left, right+1)
-            if (min == "") min = temp;
-            else min = min.length<temp.length?min:temp;
-			
-            left++;
+    
+    let count = Object.keys(set).length;
+    
+    let l = 0,
+        r = -1;
+    
+    while (r < s.length) {
+        if (count === 0) {
+            let curr = s[l];
+            if (set[curr] !== null) set[curr]++;
+            if (set[curr] > 0) count += 1;
+            // if ((r - l + 1) > min.length) min = s.substring(l, r + 1);
+            let temp = s.substring(l, r + 1)
+            if (min === ''){
+                min = temp;
+            } else {
+                if (temp.length < min.length) min = temp;
+            }
+            
+            l++;
         } else {
-            right++;
-            let current = s[right];
-			
-            if (map[current] != null) map[current]--;
-			
-            if (map[current] == 0) count--;
+            r++; 
+            let curr = s[r];
+            if (set[curr] !== null) set[curr]--;
+            if (set[curr] === 0) count--;
         }
     }
     return min;
+    
 };
