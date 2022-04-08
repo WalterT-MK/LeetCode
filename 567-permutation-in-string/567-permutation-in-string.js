5beabc998c1d3f05a66392c72310e19d076ed3cd
Walter Tang <tangtgs1516993@gmail.com>
2022-04-08 13:10:29 -0700

    Time: 120 ms (64.18%), Space: 44.2 MB (68.84%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -4,7 +4,7 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    if (s2.length < s1.length) return false;
+    if (s1.length > s2.length) return false;
     
     let arr1 = new Array(26).fill(0);
     let arr2 = new Array(26).fill(0);
@@ -13,31 +13,29 @@ var checkInclusion = function(s1, s2) {
         arr1[s1[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
         arr2[s2[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
     }
+    let l = 0;
     let matches = 0;
     for (let i = 0; i < 26; i++) {
-        if(arr1[i] === arr2[i]) matches++;
+        if (arr1[i] === arr2[i]) matches+=1;
     }
-    let l = 0;
     for (let r = s1.length; r < s2.length; r++) {
         if (matches === 26) return true;
         
         let index = s2[r].charCodeAt(0) - 'a'.charCodeAt(0);
-        arr2[index] +=1;
-        if (arr1[index] === arr2[index]) {
-            matches++;
-        } else if (arr1[index] + 1 === arr2[index]) {
-            matches--;
-        }
+        
+        arr2[index] += 1;
+        if (arr1[index] === arr2[index]) matches += 1;
+        else if (arr1[index] + 1 === arr2[index]) matches -= 1;
         
         index = s2[l].charCodeAt(0) - 'a'.charCodeAt(0);
-        arr2[index] -=1;
-        if (arr1[index] === arr2[index]) {
-            matches++;
-        } else if (arr1[index] - 1 === arr2[index]) {
-            matches--;
-        }
+        arr2[index] -= 1;
+        if (arr1[index] === arr2[index]) matches += 1;
+        else if (arr1[index] - 1 === arr2[index]) matches -= 1;
+        
         l++;
     }
     if (matches === 26) return true;
+    
     return false;
+    
 };
