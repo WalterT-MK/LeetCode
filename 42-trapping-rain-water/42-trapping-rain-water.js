c69982717dc5e4f0bc97567ad90ed19a453bed1f
Walter Tang <tangtgs1516993@gmail.com>
2022-04-07 23:15:49 -0700

    Time: 92 ms (56.42%), Space: 42.6 MB (85.51%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -3,27 +3,24 @@
  * @return {number}
  */
 var trap = function(height) {
-    let l = 0;
-    let r = height.length - 1;
-    let lmax = 0;
-    let rmax = 0;
-    let res = 0;
-    while (l<r) {
+    let l = 0,
+        r = height.length - 1;
+    
+    let area = 0;
+    let lmax = 0, 
+        rmax = 0;
+    while (l < r) {
         lmax = Math.max(lmax, height[l]);
-        if(height[l] < lmax) {
-            res+= lmax - height[l];
-        }
+        area += lmax - height[l];
         
         rmax = Math.max(rmax, height[r]);
-        if(height[r] < rmax) {
-            res+= rmax - height[r];
-        }
+        area+= rmax - height[r];
         
-        if (height[l] < height[r]) {
-            l++;
-        } else {
+        if (height[l] > height[r]) {
             r--;
+        } else {
+            l++;
         }
     }
-    return res;
+    return area;
 };
