2434a332450b3cf250e6bed8a92a973d06bd349f
Walter Tang <tangtgs1516993@gmail.com>
2022-04-09 11:28:12 -0700

    Time: 95 ms (49.28%), Space: 44.6 MB (14.44%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -13,12 +13,13 @@
 var mergeTwoLists = function(list1, list2) {
     let node = new ListNode(null);
     let curr = node;
+    
     while (list1 && list2) {
         if (list1.val > list2.val) {
             curr.next = list2;
             list2 = list2.next;
         } else {
-            curr.next = list1; 
+            curr.next = list1;
             list1 = list1.next;
         }
         curr = curr.next;
