00821d08050b53dcc373097a28c519fc1e76d41d
Walter Tang <tangtgs1516993@gmail.com>
2022-04-08 17:00:43 -0700

    Time: 80 ms (54.91%), Space: 42.7 MB (15.04%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,14 +4,12 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    var l = 0;
-    var r = nums.length - 1;
+    let l = 0,
+        r = nums.length - 1;
     
     while (l <= r) {
-        var mid = Math.floor((l+r)/2);
-        if (nums[mid] === target) {
-            return mid;
-        }
+        let mid = Math.floor((l+r) / 2);
+        if (nums[mid] === target) return mid; 
         if (nums[l] <= nums[mid]) {
             if (nums[l] <= target && target <= nums[mid]) {
                 r = mid - 1;
@@ -19,11 +17,11 @@ var search = function(nums, target) {
                 l = mid + 1;
             }
         } else {
-            if (nums[mid] <= target && target <= nums[r]) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
+             if (nums[mid] <= target && target <= nums[r]) {
+                 l = mid + 1;
+             } else {
+                 r = mid - 1;
+             }
         }
     }
     return -1;
