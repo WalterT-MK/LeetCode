699ce05478d703e3f02e971b02bd5d07d71a2a37
Walter Tang <tangtgs1516993@gmail.com>
2022-04-09 23:37:52 -0700

    Time: 106 ms (43.20%), Space: 44.8 MB (69.13%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -4,24 +4,24 @@
  */
 var evalRPN = function(tokens) {
     let stack = [];
-    
+    let num1, num2;
     for (let i = 0; i < tokens.length; i++) {
         let curr = tokens[i];
         if (curr === '+') {
-            let num1 = Number(stack.pop());
-            let num2 = Number(stack.pop());
+            num1 = Number(stack.pop());
+            num2 = Number(stack.pop());
             stack.push(num1 + num2);
         } else if (curr === '-') {
-            let num1 = Number(stack.pop());
-            let num2 = Number(stack.pop());
+            num1 = Number(stack.pop());
+            num2 = Number(stack.pop());
             stack.push(num2 - num1);
         } else if (curr === '*') {
-            let num1 = Number(stack.pop());
-            let num2 = Number(stack.pop());
+            num1 = Number(stack.pop());
+            num2 = Number(stack.pop());
             stack.push(num1 * num2);
         } else if (curr === '/') {
-            let num1 = Number(stack.pop());
-            let num2 = Number(stack.pop());
+            num1 = Number(stack.pop());
+            num2 = Number(stack.pop());
             stack.push(~~(num2/num1));
         } else {
             stack.push(curr);
