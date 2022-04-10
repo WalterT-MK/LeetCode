344e8d10d4c02ebf0377ff674ca0ce9781404333
Walter Tang <tangtgs1516993@gmail.com>
2022-04-10 15:40:06 -0700

    Time: 56 ms (99.67%), Space: 44.7 MB (95.80%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,8 +11,6 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (root === undefined || root === null) {
-        return 0;
-    }
+    if (!root) return 0;
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
