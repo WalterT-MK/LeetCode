294550f19740c661da56348f27e6df9645dd0eed
Walter Tang <tangtgs1516993@gmail.com>
2022-04-09 23:36:00 -0700

    Time: 1473 ms (5.02%), Space: 53.2 MB (5.53%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack = [];
+    
+    for (let i = 0; i < tokens.length; i++) {
+        let curr = tokens[i];
+        if (curr === '+') {
+            let num1 = Number(stack.pop());
+            let num2 = Number(stack.pop());
+            stack.push(num1 + num2);
+        } else if (curr === '-') {
+            let num1 = Number(stack.pop());
+            let num2 = Number(stack.pop());
+            stack.push(num2 - num1);
+        } else if (curr === '*') {
+            let num1 = Number(stack.pop());
+            let num2 = Number(stack.pop());
+            stack.push(num1 * num2);
+        } else if (curr === '/') {
+            let num1 = Number(stack.pop());
+            let num2 = Number(stack.pop());
+            stack.push(~~(num2/num1));
+        } else {
+            stack.push(curr);
+        }
+        console.log(stack)
+    }
+    return stack[0]
+};
