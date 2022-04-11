719aa6201fcdebdf46c06a11e53cd449e16f9f92
Walter Tang <tangtgs1516993@gmail.com>
2022-04-10 17:28:22 -0700

    Time: 144 ms (48.43%), Space: 120.1 MB (30.11%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -13,14 +13,17 @@
  */
 var buildTree = function(preorder, inorder) {
     if (preorder.length === 0 || inorder.length === 0) return null;
-    var root = new TreeNode(preorder[0]);
-    var index = inorder.indexOf(preorder[0]);//3
-    var leftPre = preorder.slice(1, index+1);
-    var leftIn = inorder.slice(0,index);
+    let rootVal = preorder[0];
+    let root = new TreeNode(rootVal);
+    let index = inorder.indexOf(rootVal);
     
-    var rightPre = preorder.slice(index + 1);
-    var rightIn = inorder.slice(index+1);
+    let leftPre = preorder.slice(1,index + 1);
+    let leftIn = inorder.slice(0, index);
+    
+    let rightPre = preorder.slice(index + 1);
+    let rightIn = inorder.slice(index + 1);
     root.left = buildTree(leftPre, leftIn);
     root.right = buildTree(rightPre, rightIn);
     return root;
+    
 };
