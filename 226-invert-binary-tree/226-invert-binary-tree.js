ff2ece3ab344fa0fe70026abed9d3af09573630b
Walter Tang <tangtgs1516993@gmail.com>
2022-04-11 20:21:39 -0700

    Time: 82 ms (48.94%), Space: 42.4 MB (63.94%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -11,9 +11,9 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    if (root === null) {
-        return root;
-    }
-    [root.left, root.right] = [invertTree(root.right), invertTree(root.left)]
+    if (root === null) return root;
+    [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
     return root;
+    
+    
 };
