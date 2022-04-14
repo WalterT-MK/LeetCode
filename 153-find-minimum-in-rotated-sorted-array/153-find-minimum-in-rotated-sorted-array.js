a1da56153a9f365948bd0ff0003555aef14a7023
Walter Tang <tangtgs1516993@gmail.com>
2022-04-13 20:46:15 -0700

    Time: 59 ms (92.87%), Space: 42.4 MB (32.81%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -3,11 +3,10 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let l = 0,
-        r = nums.length - 1; 
-    
+    let l = 0; 
+    let r = nums.length - 1;
     while (l < r) {
-        let mid = Math.floor ((l+r) / 2);
+        let mid = Math.floor((l+r) / 2);
         if (nums[mid] > nums[r]) {
             l = mid + 1;
         } else {
