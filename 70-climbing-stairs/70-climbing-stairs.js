afcc6af817c7c0094ac2516ececd90ec28a8dec0
Walter Tang <tangtgs1516993@gmail.com>
2022-04-15 16:16:05 -0700

    Time: 63 ms (81.75%), Space: 42.1 MB (41.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,27 +2,12 @@
  * @param {number} n
  * @return {number}
  */
-// var climbStairs = function(n) {
-//     var dp = new Array(n+1);
-//     dp[1] = 1;
-//     dp[2] = 2;
-//     for (var i = 3; i <= n; i++) {
-//         dp[i] = dp[i-1] + dp[i-2];
-//     }
-//     return dp[n];
-// };
-//---------------------------------
-var climbStairs = function(n, memo = new Array()) {
-    if (n === 1) {
-        return 1;
+var climbStairs = function(n) {
+    let dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i < dp.length; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
-    if (n === 2) {
-        return 2;
-    }
-    if (memo[n] != undefined) {
-        return memo[n];
-    }
-    var res = climbStairs(n-1, memo) + climbStairs(n-2, memo);
-    memo[n] = res;
-    return res;
+    return dp[n];
 };
