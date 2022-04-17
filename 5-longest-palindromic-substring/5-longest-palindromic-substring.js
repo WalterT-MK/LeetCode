64a473d49fcf10db8206994b594ef77abca8131f
Walter Tang <tangtgs1516993@gmail.com>
2022-04-17 16:26:56 -0700

    Time: 140 ms (70.86%), Space: 48 MB (49.26%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -3,29 +3,40 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
-    const checkPalin = (str, i, j) => {
-        while (i >= 0 && j < str.length && str[i] === str[j]) {
-            i--;
-            j++;
+    //double pointer start from the middle
+    let ans = '';
+    //if ans.length is odd number;
+    for (let i = 0; i < s.length; i++) {
+        let l = i;
+        let r = i;
+        while (l >= 0 && r <s.length) {
+            if (s[l] === s[r]) {
+                let temp = s.slice(l, r+1);
+                if (temp.length > ans.length) {
+                    ans = temp;
+                }
+                l--;
+                r++;
+            } else {
+                break;
+            }
         }
-        return str.slice(i + 1, j);
     }
-    
-    
-    
-    var ans = '';
-    var longest = '';
-    for (var i = 0; i < s.length; i++) {
-        var substring1 = checkPalin(s, i, i);
-        var substring2 = checkPalin(s, i, i+1);
-        if (substring1.length > substring2.length) {
-            longest = substring1;
-        } else {
-            longest = substring2;
-        }
-        if (longest.length > ans.length) {
-            ans = longest;
+    //if ans.length is even number;
+    for (let i = 0; i < s.length - 1; i++) {
+        let l = i;
+        let r = i + 1;
+        while (l >= 0 && r <s.length) {
+            if (s[l] === s[r]) {
+                let temp = s.slice(l, r+1);
+                if (temp.length > ans.length) {
+                    ans = temp;
+                }
+                l--;
+                r++;
+            } else {
+                break;
+            }
         }
     }
     return ans;
