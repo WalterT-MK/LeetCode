87521e8657ce190cd157d9f55f4c84c8e13870f2
Walter Tang <tangtgs1516993@gmail.com>
2022-04-16 19:14:24 -0700

    Time: 89 ms (33.79%), Space: 42.2 MB (42.43%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.js
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -3,21 +3,16 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length === 1) {
-        return nums[0];
+    let helper = (arr) => {
+        let dp = new Array(arr.length);
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+        for (let i = 2; i < arr.length; i++) {
+            dp[i] = Math.max(arr[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[arr.length - 1];
     }
-    if(nums.length === 2) {
-        return Math.max(nums[0], nums[1]);
-    }
-    
-    return Math.max(helper(nums.slice(0,-1)), helper(nums.slice(1)));
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+    return Math.max(helper(nums.slice(0, nums.length - 1)), helper(nums.slice(1)));
 };
-function helper (nums) {
-    var dp = new Array(nums.length);
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
-    for (var i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
-    }
-    return dp[nums.length - 1];
-}
