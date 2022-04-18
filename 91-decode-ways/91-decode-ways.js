5a90988bcfde052c75871fcc17c271f5f2b62ab2
Walter Tang <tangtgs1516993@gmail.com>
2022-04-17 20:11:27 -0700

    Time: 67 ms (89.27%), Space: 43.2 MB (34.92%) - LeetHub

--- 91-decode-ways/91-decode-ways.js
+++ 91-decode-ways/91-decode-ways.js
@@ -2,6 +2,24 @@
  * @param {string} s
  * @return {number}
  */
+// var numDecodings = function(s) {
+//     if (s.length < 1) return 0;
+//     let memo = [];
+    
+//     let dp = (index) => {
+//         let count = 0;
+//         if (index === s.length) return 1;
+//         if (memo[index] != null) return memo[index];
+//         if (s[index] > 0) count += dp(index + 1);
+//         if (s[index] !== 0 && s[index + 1] !== null && s[index] + s[index + 1] < 27) {
+//             count += dp(index + 2);
+//         }
+//         memo[index] = count;
+//         return count;
+//     }
+    
+//     return dp(0);
+// };
 var numDecodings = function(s) {
     if (s.length < 1) {
         return 0;
