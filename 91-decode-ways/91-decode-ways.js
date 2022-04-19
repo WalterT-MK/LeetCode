b9a509b30bf2b3bf8f08c8479c9fd64535c1efb7
Walter Tang <tangtgs1516993@gmail.com>
2022-04-18 19:59:56 -0700

    Time: 55 ms (99.52%), Space: 43.3 MB (34.61%) - LeetHub

--- 91-decode-ways/91-decode-ways.js
+++ 91-decode-ways/91-decode-ways.js
@@ -2,47 +2,19 @@
  * @param {string} s
  * @return {number}
  */
-// var numDecodings = function(s) {
-//     if (s.length < 1) return 0;
-//     let memo = [];
-    
-//     let dp = (index) => {
-//         let count = 0;
-//         if (index === s.length) return 1;
-//         if (memo[index] != null) return memo[index];
-//         if (s[index] > 0) count += dp(index + 1);
-//         if (s[index] !== 0 && s[index + 1] !== null && s[index] + s[index + 1] < 27) {
-//             count += dp(index + 2);
-//         }
-//         memo[index] = count;
-//         return count;
-//     }
-    
-//     return dp(0);
-// };
 var numDecodings = function(s) {
-    if (s.length < 1) {
-        return 0;
+    if (s.length < 1) return 0;
+    let memo = [];
+    let dp = (index) => {
+        let count = 0;
+        if (index === s.length) return 1;
+        if (memo[index] != null) return memo[index];
+        if (s[index] > 0) count += dp(index + 1);
+        if (s[index] != 0 && s[index + 1] != null && s[index] + s[index + 1] < 27) {
+            count += dp(index + 2);
+        }
+        memo[index] = count;
+        return count;
     }
-    var memo = [];
-    var helper = (index) => {
-        var result = 0;
-        if (index === s.length) {
-            return 1;
-        }
-        if (memo[index] != null) {
-            return memo[index];
-        }
-        if (s[index] > 0) {
-            result += helper(index + 1);
-        }
-        if (s[index] != 0 && 
-            s[index + 1] != null && 
-            s[index]+s[index+1] < 27) {
-            result += helper(index + 2);
-        }
-        memo[index] = result;
-        return result;
-    }
-    return helper(0);
+    return dp(0);
 };
