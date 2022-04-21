42b629e763ac59ef3fca7039ebe7cc59dd8206d2
Walter Tang <tangtgs1516993@gmail.com>
2022-04-21 15:22:00 -0700

    Time: 108 ms (59.50%), Space: 50.1 MB (34.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,29 +3,23 @@
  * @return {number}
  */
 var numIslands = function(grid) {
+    let land = '1';
+    let water = '0';
     if (!grid) return 0;
     let rows = grid.length;
     let cols = grid[0].length;
-    let visit = new Set();
+    // let visit = new Set();
     let count = 0;
     let directions = [[1,0], [-1,0], [0,1], [0,-1]];
     
     const bfs = (r, c) => {
-        let q = [];
-        visit.add(JSON.stringify([r,c]));
-        q.push([r,c]);
+        let q = [[r,c]];
         while (q.length !== 0) {
             let [row, col] = q.shift();
+            if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == water) continue;
+            grid[row][col] = water;
             for (let [dr, dc] of directions) {
-                let iRow = row + dr;
-                let iCol = col + dc;
-                if ((iRow >= 0 && iRow < rows) &&
-                    (iCol >= 0 && iCol < cols) &&
-                    visit.has(JSON.stringify([iRow, iCol])) !== true &&
-                    grid[iRow][iCol] === '1') {
-                    q.push([iRow, iCol]);
-                    visit.add(JSON.stringify([iRow, iCol]));
-                }
+                q.push([row + dr, col + dc]);
             }
         }
     }
@@ -33,7 +27,8 @@ var numIslands = function(grid) {
     
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
-            if (grid[r][c] === '1' && visit.has(JSON.stringify([r,c])) !== true) {
+            // if (grid[r][c] === '1' && visit.has(JSON.stringify([r,c])) !== true) {
+            if (grid[r][c] === land) {
                 bfs(r,c);
                 
                 count += 1;
