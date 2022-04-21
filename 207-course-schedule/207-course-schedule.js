1ff571f9352e70560334b229319112db8898be57
Walter Tang <tangtgs1516993@gmail.com>
2022-04-21 13:10:24 -0700

    Time: 102 ms (63.27%), Space: 48.6 MB (33.74%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    
+    let map = new Map();
+    for (let i = 0; i < numCourses; i++) {
+        map.set(i,[]);
+    }
+    for (let [course, prereq] of prerequisites) {
+        map.get(course).push(prereq);
+    }
+    
+    let visit = new Set();
+    const dfs = (course) => {
+        if (visit.has(course)) return false;
+        if (map.get(course) === []) return true;
+        
+        visit.add(course);
+        for (let pre of map.get(course)) {
+            if (!dfs(pre)) return false;
+        }
+        visit.delete(course);
+        map.set(course, []);
+        return true;
+    }
+    for (let i = 0; i < numCourses; i++) {
+        if (!dfs(i)) return false;
+    }
+    return true;
+};
