281e426435a783653d28bae9ed4ea3b62e749e8d
Walter Tang <tangtgs1516993@gmail.com>
2022-04-23 21:43:04 -0700

    Time: 68 ms (96.92%), Space: 44.6 MB (75.59%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -5,37 +5,31 @@
 var orangesRotting = function(grid) {
     let rows = grid.length;
     let cols = grid[0].length;
+    let orange = 0;
+    let time = 0;
+    let directions = [[0,1], [1,0], [-1,0], [0,-1]];
     let q = [];
-    let directions = [[0,1], [0,-1], [1,0], [-1,0]];
-    let countMin = 0;
-    let countOrange = 0;
     
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
-            if (grid[r][c] === 2) q.push([r,c, 0]);
-            if (grid[r][c] === 1) countOrange += 1;
+            if (grid[r][c] === 1) orange += 1;
+            if (grid[r][c] === 2) q.push([r,c,0]);
         }
     }
     
-    while (q.length !== 0 && countOrange) {
+    while (q.length !== 0 && orange) {
         let [row, col, mins] = q.shift();
-        
         if (grid[row][col] === 1) {
             grid[row][col] = 2;
-            countMin = mins;
-            countOrange -= 1;
+            orange -= 1;
+            time = mins;
         }
-        
-        for (let [dr, dc] of directions) {
-            let [newR, newC] = [row + dr, col +dc];
+        for (let [dr,dc] of directions) {
+            let [newR, newC] = [row+dr, col+dc];
             if (newR < 0 || newR >= rows || newC < 0 || newC >= cols) continue;
-            if (grid[newR][newC] === 1) q.push([newR, newC, mins + 1]);
+            if (grid[newR][newC] === 1) q.push([newR, newC, mins+1]);
         }
-        
     }
-
-    return countOrange ? -1 : countMin;
     
-    
-    
+    return orange ? -1 : time;
 };
