dc4626a7ca658f0ee633a786044103507b6e73d2
Walter Tang <tangtgs1516993@gmail.com>
2022-04-26 14:45:07 -0700

    Time: 134 ms (42.24%), Space: 45.6 MB (99.51%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -3,19 +3,14 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    var idx = 0;
-    var max = 0;
-    var target = nums.length - 1;
-    
-    while (idx < nums.length) {
-        max = Math.max(max, idx + nums[idx]);
-        if (max >= target) {
-            return true;
-        }
-        if (max <= idx && nums[idx] === 0) {
-            return false;
-        }
-        idx++;
+    let target = nums.length - 1;
+    let index = 0;
+    let max = 0;
+    while (index < nums.length) {
+        max = Math.max(max, index + nums[index]);
+        if (max >= target) return true;
+        if (nums[index] === 0 && max <= index) return false;
+        index += 1;
     }
     return false;
 };
