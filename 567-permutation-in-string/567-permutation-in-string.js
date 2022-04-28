d3353bfea8d81901dd295bb7293221f6a74d3e8d
Walter Tang <tangtgs1516993@gmail.com>
2022-04-27 21:21:18 -0700

    Time: 146 ms (54.73%), Space: 43.7 MB (77.53%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -4,37 +4,36 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    if (s1.length > s2.length) return false;
     
-    let arr1 = new Array(26).fill(0);
-    let arr2 = new Array(26).fill(0);
+    if(s1.length > s2.length) return false;
+    let count1 = new Array(26).fill(0);
+    let count2 = new Array(26).fill(0);
     
     for (let i = 0; i < s1.length; i++) {
-        arr1[s1[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
-        arr2[s2[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        count1[s1[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        count2[s2[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+    }
+    let match = 0;
+    for (let i = 0; i < 26; i++) {
+        if (count1[i] === count2[i]) match += 1;
     }
     let l = 0;
-    let matches = 0;
-    for (let i = 0; i < 26; i++) {
-        if (arr1[i] === arr2[i]) matches+=1;
-    }
-    for (let r = s1.length; r < s2.length; r++) {
-        if (matches === 26) return true;
+    for (let i = s1.length; i < s2.length; i++) {
+        if (match === 26) return true;
+        let index = s2[i].charCodeAt(0) - 'a'.charCodeAt(0);
         
-        let index = s2[r].charCodeAt(0) - 'a'.charCodeAt(0);
-        
-        arr2[index] += 1;
-        if (arr1[index] === arr2[index]) matches += 1;
-        else if (arr1[index] + 1 === arr2[index]) matches -= 1;
+        count2[index] += 1;
+        if (count2[index] === count1[index]) match += 1;
+        else if (count1[index] + 1 === count2[index]) match -= 1;
         
         index = s2[l].charCodeAt(0) - 'a'.charCodeAt(0);
-        arr2[index] -= 1;
-        if (arr1[index] === arr2[index]) matches += 1;
-        else if (arr1[index] - 1 === arr2[index]) matches -= 1;
+        count2[index] -= 1;
+        if (count2[index] === count1[index]) match += 1;
+        else if (count2[index] + 1 === count1[index]) match -= 1;
         
-        l++;
+        l += 1;
     }
-    if (matches === 26) return true;
+    if (match === 26) return true;
     
     return false;
     
