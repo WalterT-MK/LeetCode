148fd5b44b6cd3ba733fc1358bd8976f4383d8ca
Walter Tang <tangtgs1516993@gmail.com>
2022-04-29 14:51:55 -0700

    Time: 380 ms (87.26%), Space: 84.1 MB (66.47%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.js
+++ 239-sliding-window-maximum/239-sliding-window-maximum.js
@@ -5,21 +5,15 @@
  */
 var maxSlidingWindow = function(nums, k) {
     let q = [];
-    let ans = []
-    
-    for (let i = 0; i < nums.length; i++) {
+    let res = [];
+    for (let i = 0; i < nums.length; i += 1) {
         while (q && nums[q[q.length - 1]] <= nums[i]) {
             q.pop();
         }
         q.push(i);
         
-        if (q[0] === i - k) {
-            q.shift();
-        }
-        
-        if (i >= k - 1) {
-            ans.push(nums[q[0]]);
-        }
+        if (q[0] === i - k) q.shift();
+        if (i >= k - 1) res.push(nums[q[0]]);
     }
-    return ans;
+    return res;
 };
