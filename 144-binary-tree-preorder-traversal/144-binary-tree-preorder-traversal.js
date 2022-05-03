2c949f92f6fc9082c8cec2ce06babe251b4ea788
Walter Tang <tangtgs1516993@gmail.com>
2022-05-02 18:26:55 -0700

    Time: 80 ms (43.17%), Space: 42 MB (85.73%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -11,13 +11,27 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
+    // let res = [];
+    // const traver = (root) => {
+    //     if(!root) return;
+    //     res.push(root.val);
+    //     if (root.left) traver(root.left);
+    //     if (root.right) traver(root.right);
+    // }
+    // traver(root);
+    // return res;
+    
+    if (!root) return [];
+    let q = [root];
     let res = [];
-    const traver = (root) => {
-        if(!root) return;
-        res.push(root.val);
-        if (root.left) traver(root.left);
-        if (root.right) traver(root.right);
+    
+    while (q.length) {
+        let curr = q.pop();
+        res.push(curr.val);
+        if (curr.right) q.push(curr.right);
+        if (curr.left) q.push(curr.left);
     }
-    traver(root);
     return res;
+    
+
 };
