6359abcad8bc1a99e20607894301551441d3bd1a
Walter Tang <tangtgs1516993@gmail.com>
2022-05-03 17:23:56 -0700

    Time: 218 ms (45.16%), Space: 52.1 MB (86.00%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,40 +3,25 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    var ans = [];
-    if (nums.length < 3) {
-        return ans;
-    }
+    
+    let res = [];
     nums.sort((a,b) => a - b);
-    for (let i = 0; i < nums.length-2; i++) {
-        if (nums[i] > 0) {
-            break;
-        }
-        if (i>0 && nums[i] === nums[i-1]) {
-            continue;
-        }
-        let l = i + 1; 
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] > 0) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let l = i + 1;
         let r = nums.length - 1;
         while (l < r) {
             let sum = nums[i] + nums[l] + nums[r];
-            if(sum === 0){
-                ans.push([nums[i], nums[l], nums[r]]);
-                while(nums[l] === nums[l+1]) {
-                    l++;
-                }
-                while(nums[r] === nums[r-1]) {
-                    r--;
-                }
-                l++;
-                r--;
-            } else if (sum > 0) {
-                r--;
-            } else {
-                l++;
-            }
-
+            if (sum === 0){
+                res.push([nums[i], nums[l], nums[r]]);
+                while (nums[l] === nums[l + 1]) l += 1;
+                while (nums[r] === nums[r - 1]) r -= 1;
+                l += 1, r -= 1;
+            } 
+            else if (sum > 0) r -= 1;
+            else l += 1;
         }
     }
-    return ans;
-
+    return res;
 };
