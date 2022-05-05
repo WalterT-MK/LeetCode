b23e610f5f89d3d965d8a82dc0750d799bca631b
Walter Tang <tangtgs1516993@gmail.com>
2022-05-05 00:00:56 -0700

    Time: 108 ms (24.81%), Space: 47.1 MB (5.26%) - LeetHub

--- 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.js
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.js
@@ -12,6 +12,8 @@
  * @return {number}
  */
 var closestValue = function(root, target) {
+    //brute force, calculate all nodes' different with target, sort
+    //time: O(nlog(n)) space: O(n)
     let obj = [];
     const dfs = (node) => {
         if (node.left) dfs(node.left);
@@ -19,6 +21,16 @@ var closestValue = function(root, target) {
         if (node.right) dfs(node.right);
     }
     dfs(root);
-    obj.sort((a,b) => a[1] - b[1]);
-    return obj[0][0];
+    // obj.sort((a,b) => a[1] - b[1]);
+    if (obj.length === 1 || target < obj[0][0]) return obj[0][0];
+    for (let i = 1; i < obj.length; i++) {
+        if (target >= obj[i - 1][0] && target <= obj[i][0]) {
+            if (obj[i - 1][1] > obj[i][1]) {
+                return obj[i][0]
+            } else {
+                return obj[i-1][0];
+            }
+        }
+    }
+    return obj[obj.length - 1][0];
 };
