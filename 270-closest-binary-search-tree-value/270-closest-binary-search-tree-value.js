a884c2db533f1cf39e85ad29f239c8dcc8b96b42
Walter Tang <tangtgs1516993@gmail.com>
2022-05-05 00:03:45 -0700

    Time: 103 ms (31.58%), Space: 46.1 MB (22.05%) - LeetHub

--- 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.js
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.js
@@ -14,23 +14,33 @@
 var closestValue = function(root, target) {
     //brute force, calculate all nodes' different with target, sort
     //time: O(nlog(n)) space: O(n)
-    let obj = [];
+    /*let obj = [];
     const dfs = (node) => {
         if (node.left) dfs(node.left);
         obj.push([node.val, Math.abs(node.val - target)]);
         if (node.right) dfs(node.right);
     }
     dfs(root);
-    // obj.sort((a,b) => a[1] - b[1]);
-    if (obj.length === 1 || target < obj[0][0]) return obj[0][0];
+    obj.sort((a,b) => a[1] - b[1]);
+    return obj[0][0]*/
+    
+    
+    let obj = [];
+    const dfs = (node) => {
+        if (node.left) dfs(node.left);
+        obj.push(node.val);
+        if (node.right) dfs(node.right);
+    }
+    dfs(root);
+    if (obj.length === 1 || target < obj[0]) return obj[0];
     for (let i = 1; i < obj.length; i++) {
-        if (target >= obj[i - 1][0] && target <= obj[i][0]) {
-            if (obj[i - 1][1] > obj[i][1]) {
-                return obj[i][0]
+        if (target >= obj[i - 1] && target <= obj[i]) {
+            if (Math.abs(target - obj[i - 1]) > Math.abs(target - obj[i])) {
+                return obj[i]
             } else {
-                return obj[i-1][0];
+                return obj[i-1];
             }
         }
     }
-    return obj[obj.length - 1][0];
+    return obj[obj.length - 1];
 };
