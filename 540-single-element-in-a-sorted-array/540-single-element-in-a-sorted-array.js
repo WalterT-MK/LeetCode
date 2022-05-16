319975383076365ac0d2c54fb4a735561994df9f
Walter Tang <tangtgs1516993@gmail.com>
2022-05-15 19:25:20 -0700

    Time: 74 ms (68.07%), Space: 45 MB (65.10%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.js
@@ -3,21 +3,21 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let l = 0;
+    let l = 0; 
     let r = nums.length - 1;
     while (l < r) {
         let mid = l + Math.floor((r - l) / 2);
         if (nums[mid] === nums[mid + 1]) {
-            if ((mid - l) % 2 === 0) {
-                l = mid + 2;
-            } else {
+            if ((r - mid + 1) % 2 === 0) {
                 r = mid - 1;
+            } else {
+                l = mid + 2;
             }
         } else if (nums[mid] === nums[mid - 1]) {
-            if ((mid - 1 - l) % 2 === 0) {
-                l = mid + 1;
-            } else {
+            if ((r - mid) % 2 === 0) {
                 r = mid - 2;
+            } else {
+                l = mid + 1;
             }
         } else {
             return nums[mid];
