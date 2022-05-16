ca583806846427b71dfe94bb6751a20ec8f41578
Walter Tang <tangtgs1516993@gmail.com>
2022-05-15 18:58:52 -0700

    Time: 107 ms (18.80%), Space: 44.6 MB (93.94%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0,
-        r = nums.length - 1;
+    let l = 0; 
+    let r = nums.length - 1;
     while (l <= r) {
-        let mid = Math.floor((l + r) / 2);
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] > target) {
+        let mid = l + Math.floor((r - l) / 2);
+        if (nums[mid] > target) {
             r = mid - 1;
-        } else {
+        } else if (nums[mid] < target) {
             l = mid + 1;
+        } else {
+            return mid;
         }
     }
     return -1;
