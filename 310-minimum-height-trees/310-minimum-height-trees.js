e8b2038263f9a26870401d2460dbbc09c8a7cb3e
Walter Tang <tangtgs1516993@gmail.com>
2022-05-18 14:45:23 -0700

    Time: 305 ms (34.84%), Space: 72.9 MB (20.81%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.js
+++ 310-minimum-height-trees/310-minimum-height-trees.js
@@ -4,24 +4,34 @@
  * @return {number[]}
  */
 var findMinHeightTrees = function(n, edges) {
-    let res = [];
+    /*let res = [];
+    //edge case
     if (n < 2) {
         for (let i = 0; i < n; i++) {
             res.push(i);
         }
         return res;
     }
+    //create a map that store all connection between nodes.
     let graph = new Map();
+    
+    //an array to represent how much connection (adjancency node) have
+    //the index of array represent the value of node.
     let indegrees = new Array(n).fill(0);
+    
+    //create map, 'i' as key while array as adjancency value
     for (let i = 0; i < n; i++) {
         graph.set(i, []);
     }
+    
+    //while creating the map, also increase the count of connection of such value
     for (let [e1, e2] of edges) {
         graph.get(e1).push(e2);
         graph.get(e2).push(e1);
         indegrees[e1] += 1;
         indegrees[e2] += 1;
     }
+    //create a queue that store all leaf (lowest layer?)
     let queue = [];
     for (let i = 0; i < n; i++) {
         if (indegrees[i] === 1) {
@@ -42,6 +52,46 @@ var findMinHeightTrees = function(n, edges) {
                 }
             }
         }    
+    }
+    return queue;
+    */
+    
+    let res = [];
+    //edge case
+    if (n < 2) {
+        for (let i = 0; i < n; i++) {
+            res.push(i);
+        }
+        return res;
+    }
+    let heapMap = new Map();
+    let connections = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        heapMap.set(i, []);
+    }
+    for (let [e1,e2] of edges) {
+        heapMap.get(e1).push(e2);
+        heapMap.get(e2).push(e1);
+        
+        connections[e1] += 1;
+        connections[e2] += 1;
+    }
+    let queue = [];
+    for (let i = 0; i < n; i++) {
+        if (connections[i] === 1) queue.push(i);
+    }
+    
+    while (n > 2) {
+        let size = queue.length;
+        n -= size;
+        while (size > 0) {
+            let curr = queue.shift();
+            size -= 1;
+            for (let node of heapMap.get(curr)) {
+                connections[node] -= 1;
+                if (connections[node] === 1) queue.push(node);
+            }
+        }
     }
     return queue;
 };
