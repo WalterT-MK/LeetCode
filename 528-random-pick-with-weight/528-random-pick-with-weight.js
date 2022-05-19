6eecdab86ea2c4fdc61b313ba8cb747c42e5cd9c
Walter Tang <tangtgs1516993@gmail.com>
2022-05-18 20:02:09 -0700

    Time: 529 ms (14.56%), Space: 50.1 MB (91.49%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.js
+++ 528-random-pick-with-weight/528-random-pick-with-weight.js
@@ -4,18 +4,19 @@
 var Solution = function(w) {
     this.arry = w;
     this.weight = [];
-    this.sum = 0;
+    let sum = 0;
     for (let i = 0; i < this.arry.length; i++) {
-        this.sum += this.arry[i];
-        this.weight.push(this.sum);
+        sum += this.arry[i];
+        this.weight.push(sum);
     }
+    this.total = sum;
 };
 
 /**
  * @return {number}
  */
 Solution.prototype.pickIndex = function() {
-    let target = Math.random() * this.sum;
+    let target = Math.random() * this.total;
     for (let i = 0; i < this.weight.length; i++) {
         if (this.weight[i] > target) {
             return i;
