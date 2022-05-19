df15337d7f2d607b97b77c030eeacabe579982c3
Walter Tang <tangtgs1516993@gmail.com>
2022-05-18 22:01:27 -0700

    Time: 382 ms (5.05%), Space: 52.6 MB (5.18%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+    intervals.sort((a,b) => a[0] - b[0]);
+    let rooms = [];
+    let heapMap = new MinPriorityQueue({priority: x => x[1]});
+    let size = 0;
+    for (let i = 0; i < intervals.length; i++) {
+        if (heapMap.size() !== 0 && intervals[i][0] >= heapMap.front().element[1]) {
+            console.log(heapMap.front())
+            heapMap.dequeue();
+        }
+        heapMap.enqueue(intervals[i]);
+        if (heapMap.size() > size) size = heapMap.size();
+    }
+    return size;
+    
+};
