e33c334f14416c509ef874071b44dbc2926b0e5d
Walter Tang <tangtgs1516993@gmail.com>
2022-05-19 22:09:42 -0700

    Time: 333 ms (77.91%), Space: 111.2 MB (49.84%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} startValue
+ * @param {number} destValue
+ * @return {string}
+ */
+var getDirections = function(root, startValue, destValue) {
+    const getPath = (node, val, path = '') => {
+        if (!node) return '';
+        else if (node.val === val) return path;
+        else {
+            return getPath(node.left, val, path + 'L') + getPath(node.right, val, path + 'R');
+        }
+    }
+    
+    let startPath = getPath(root, startValue);
+    let destPath = getPath(root, destValue);
+    
+    let i = 0;
+    // for (let i = 0; i < startPath.length && i < destPath.length; i ++) {
+    //     if (startPath[i] !== destPath[i]) {
+    //         commentNode = i;
+    //         break;
+    //     }
+    // }
+    for (; i < startPath.length && i < destPath.length && startPath[i] === destPath[i]; i++);
+    let res = '';
+    for (let j = i; j < startPath.length; j++) {
+        res += 'U';
+    }
+    return res + destPath.substring(i);
+};
