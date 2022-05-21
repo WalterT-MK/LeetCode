c9da2479426dccd1e1b8f70002dde1975a23aeba
Walter Tang <tangtgs1516993@gmail.com>
2022-05-21 15:03:38 -0700

    Time: 105 ms (48.77%), Space: 45.8 MB (28.15%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -3,17 +3,43 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
+    
     intervals.sort((a,b) => a[0] - b[0]);
-    let rooms = [];
-    let heapMap = new MinPriorityQueue({priority: x => x[1]});
-    let size = 0;
-    for (let i = 0; i < intervals.length; i++) {
-        if (heapMap.size() !== 0 && intervals[i][0] >= heapMap.front().element[1]) {
-            heapMap.dequeue();
+    let arry = new miniHeap();
+    let res = 0;
+    for (let i of intervals) {
+        if (arry.size > 0 && arry.peek[1] <= i[0]) {
+            arry.pop();
         }
-        heapMap.enqueue(intervals[i]);
-        if (heapMap.size() > size) size = heapMap.size();
+        arry.insert(i);
+        res = Math.max(res, arry.size);
+        
     }
-    return size;
+    return res;
     
 };
+
+class miniHeap {
+        constructor() {
+            this.heap = [];
+        }
+        
+        insert(val) {
+            this.heap.unshift(val);
+            this.heap.sort((a,b) => a[1] - b[1]);
+        }
+
+        get size() {
+            return this.heap.length;
+        }
+
+        get peek() {
+            if (this.heap.length === 0) return null;
+            return this.heap[0];
+        }
+
+        pop() {
+            if (this.heap.length === 0) return null;
+            return this.heap.shift();
+        }
+    }
