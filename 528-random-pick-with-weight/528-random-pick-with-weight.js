95008c8a7ee01c2e67938e4550740709142bbf1d
Walter Tang <tangtgs1516993@gmail.com>
2022-05-28 11:50:01 -0700

    Time: 220 ms (59.05%), Space: 50.9 MB (55.28%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.js
+++ 528-random-pick-with-weight/528-random-pick-with-weight.js
@@ -2,10 +2,9 @@
  * @param {number[]} w
  */
 var Solution = function(w) {
-    this.arry = w;
     let sum = 0;
     this.weight = [];
-    for (let i of this.arry) {
+    for (let i of w) {
         sum += i;
         this.weight.push(sum);
     }
@@ -17,11 +16,15 @@ var Solution = function(w) {
  */
 Solution.prototype.pickIndex = function() {
     let target = Math.random() * this.sum;
-    let l = 0; let r = this.arry.length - 1;
+    let l = 0;
+    let r = this.weight.length - 1;
     while (l < r) {
         let mid = l + Math.floor((r - l) / 2);
-        if (this.weight[mid] > target) r = mid;
-        else l = mid + 1;
+        if (this.weight[mid] > target) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
     }
     return l;
 };
