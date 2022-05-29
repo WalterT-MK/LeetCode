aa1a989b3d765d70ee2c4df4e762672846febde9
Walter Tang <tangtgs1516993@gmail.com>
2022-05-29 14:34:13 -0700

    Time: 88 ms (81.01%), Space: 43.2 MB (74.52%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -7,10 +7,16 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
     
     //sorting method;
-    return JSON.stringify(s.split('').sort()) === JSON.stringify(t.split('').sort());
+    //return JSON.stringify(s.split('').sort()) === JSON.stringify(t.split('').sort());
     
     // count char method
 
-    // let sArr = new Array(26).fill(0);
-    // let tArr = new Array(26).fill(0);
+    let sArr = new Array(26).fill(0);
+    let tArr = new Array(26).fill(0);
+    
+    for (let i = 0; i < s.length; i++) {
+        sArr[s[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        tArr[t[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+    }
+    return JSON.stringify(sArr) === JSON.stringify(tArr);
 };
