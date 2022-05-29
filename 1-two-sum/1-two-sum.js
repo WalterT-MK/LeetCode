af51ae44bc7a4e64093e040775d31ed6460d9c62
Walter Tang <tangtgs1516993@gmail.com>
2022-05-29 15:04:32 -0700

    Time: 107 ms (56.21%), Space: 43.7 MB (19.82%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,13 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    var map1 = new Map();
+    let dict = new Map();
     for (let i = 0; i < nums.length; i++) {
-        if(map1.has(nums[i])) {
-            return [i, map1.get(nums[i])]
-        } else {
-            map1.set(target - nums[i], i)
-        }
-        
+        if (dict.has(nums[i])) return [i, dict.get(nums[i])];
+        dict.set(target - nums[i], i);
     }
 };
