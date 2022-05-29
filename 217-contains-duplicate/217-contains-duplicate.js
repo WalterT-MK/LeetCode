4090291ab7c7b65943d5d92f49c093ddc5b86d89
Walter Tang <tangtgs1516993@gmail.com>
2022-05-29 14:20:33 -0700

    Time: 260 ms (16.57%), Space: 51.7 MB (15.78%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,6 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
-    return nums.length !== new Set(nums).size
+    if (nums.length === 1) return false;
+    //sorting method
+    nums.sort((a,b) => a - b);
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) return true;
+    }
+    return false;
 };
