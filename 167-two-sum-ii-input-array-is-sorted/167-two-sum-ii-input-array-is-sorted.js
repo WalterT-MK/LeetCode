86357cb41d8616e015be5253bde90bd05bcc75ed
Walter Tang <tangtgs1516993@gmail.com>
2022-05-29 18:14:16 -0700

    Time: 69 ms (83.01%), Space: 43 MB (66.97%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -7,13 +7,13 @@ var twoSum = function(numbers, target) {
     let l = 0; 
     let r = numbers.length - 1;
     while (l < r) {
-        let sumLR = numbers[l] + numbers[r];
-        if (sumLR === target) return [l+1, r+1];
-        if (sumLR > target) {
-            r--;
+        let sum = numbers[l] + numbers[r];
+        if (sum === target) {
+            return [l + 1, r + 1];
+        } else if (sum > target) {
+            r -= 1;
         } else {
-            l++;
+            l += 1;
         }
     }
-    return []
 };
