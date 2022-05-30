585ae39f397e8d425d4cbb2ed3ceef75abdc1bc8
Walter Tang <tangtgs1516993@gmail.com>
2022-05-29 22:16:26 -0700

    Time: 101 ms (53.24%), Space: 49.9 MB (25.67%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var maxArea = function(height) {
+    let res = 0;
     let l = 0; 
     let r = height.length - 1;
-    let max = 0
+    let base = height.length - 1;
     while (l < r) {
-        let currMax = Math.min(height[l], height[r]) * (r - l);
-        if(currMax > max) max = currMax;
-        if(height[l] > height[r]) {
-            r--;
-        } else {
-            l++;
-        }
+        let area = Math.min(height[l], height[r]) * base;
+        if (height[l] > height[r]) r -= 1;
+        else l += 1;
+        
+        base -= 1;
+        if (area > res) res = area;
     }
-    return max;
+    return res;
 };
