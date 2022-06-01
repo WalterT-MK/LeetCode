e9c5c3697d3c2a2fda6df8cb9663e88140924dea
Walter Tang <tangtgs1516993@gmail.com>
2022-05-31 19:28:18 -0700

    Time: 92 ms (96.84%), Space: 46.2 MB (63.66%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -5,30 +5,31 @@
  */
 var findAnagrams = function(s, p) {
     if (p.length > s.length) return [];
-    let count1 = new Array(26).fill(0);
-    let count2 = new Array(26).fill(0);
-    let match = 0;
-    let l = 0;
     let res = [];
+    let charS = new Array(26).fill(0);
+    let charP = new Array(26).fill(0);
     
     for (let i = 0; i < p.length; i++) {
-        count1[p[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
-        count2[s[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        charS[s[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        charP[p[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
     }
+    let match = 0;
     for (let i = 0; i < 26; i++) {
-        if (count1[i] === count2[i]) match += 1;
+        if (charS[i] === charP[i]) match += 1;
     }
     if (match === 26) res.push(0);
-    for (let r = p.length; r < s.length; r++) {
-        let index = s[r].charCodeAt(0) - 'a'.charCodeAt(0);
-        count2[index] += 1;
-        if (count1[index] === count2[index]) match += 1;
-        else if (count1[index] + 1 === count2[index]) match -= 1;
+    let l = 0;
+    for (let i = p.length; i < s.length; i++) {
+        let curr = s[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        charS[curr] += 1;
+        if (charS[curr] === charP[curr]) match += 1;
+        if (charS[curr] - 1 === charP[curr]) match -= 1;
         
-        index = s[l].charCodeAt(0) - 'a'.charCodeAt(0);
-        count2[index] -= 1;
-        if (count1[index] === count2[index]) match += 1;
-        else if (count1[index] - 1 === count2[index]) match -= 1;
+        curr = s[l].charCodeAt(0) - 'a'.charCodeAt(0);
+        charS[curr] -= 1;
+        if (charS[curr] === charP[curr]) match += 1;
+        if (charS[curr] + 1 === charP[curr]) match -= 1;
+        
         l += 1;
         if (match === 26) res.push(l);
     }
