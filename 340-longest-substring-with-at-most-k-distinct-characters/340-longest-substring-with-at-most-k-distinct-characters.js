31453d0e65957013952387b4683e452b48ffdfc8
Walter Tang <tangtgs1516993@gmail.com>
2022-05-31 17:38:52 -0700

    Time: 90 ms (81.63%), Space: 44.8 MB (63.02%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
@@ -5,29 +5,25 @@
  */
 var lengthOfLongestSubstringKDistinct = function(s, k) {
     if (k === 0) return 0;
-    let l = 0;
-    let distinct = 0;
-    let count = {};
+    let chars = new Map();
     let res = 0;
-    
+    let l = 0;
+    let count = 0;
     for (let i = 0; i < s.length; i++) {
-        if (count[s[i]]) {
-            count[s[i]] += 1;
+        if (chars.has(s[i])) {
+            chars.set(s[i], chars.get(s[i]) + 1);
         } else {
-            count[s[i]] = 1;
-            distinct += 1;
+            chars.set(s[i], 1);
+            count += 1;
         }
-        if (distinct <= k && (i - l + 1) > res) res = i - l + 1; 
-        while (distinct > k) {
-            if (count[s[l]] !== 1) {
-                count[s[l]] -= 1;
-            } else {
-                delete count[s[l]];
-                distinct -= 1;
-                
+        if (count <= k && i - l + 1 > res) res = i - l + 1;
+        while (count > k) {
+            chars.set(s[l], chars.get(s[l]) - 1);
+            if (chars.get(s[l]) === 0) {
+                chars.delete(s[l]);
+                count -= 1;
             }
             l += 1;
-            if (distinct <= k && (i - l + 1) > res) res = i - l + 1; 
         }
     }
     return res;
